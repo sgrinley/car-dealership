e639e0c96ae89c88e5eb6b15af8ef60a5e3b1e0e
sgrinley <shamar808@yahoo.com>
2026-05-08 12:41:42 -0700

    feat: Generate Constructor

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -11,4 +11,16 @@ public class Vehicle {
     private String color;
     private int odometer;
     private double price;
+
+    //  Generate Constructor
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 }
