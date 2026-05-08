5b9f1f037b390115daf27d94dd04896cc9eed798
sgrinley <shamar808@yahoo.com>
2026-05-08 12:38:36 -0700

    feat: Add data fields

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Vehicle {
+
+    //  Add: Data Fields (Variables)
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
 }
